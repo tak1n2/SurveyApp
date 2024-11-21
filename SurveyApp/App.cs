5c1845a742a0390e8f7bd24291bc78e45c8a5f03
Demydov Maksym <maksimdemidov05@gmail.com>
2024-11-21 17:43:33 +0200

    Added Registration and Login methods, speaking with server. Added pointers of tcpClient to login form and app form, moved connection form login to register form, remade connection method to make it async, added to all forms parametrs to receive tcpclient. Client class  - made receiveMessageAsync public

--- SurveyApp/App.cs
+++ SurveyApp/App.cs
@@ -7,15 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClientForApp;
 
 namespace SurveyApp
 {
     public partial class App : Form
     {
-        public App()
+        private readonly Client tcpClient;
+        public App(Client client)
         {
             InitializeComponent();
-
+            tcpClient = client;
         }
 
         private void App_Load(object sender, EventArgs e)
--- SurveyApp/Register.cs
+++ SurveyApp/Register.cs
@@ -1,27 +1,86 @@
+using ClientForApp;
+
 namespace SurveyApp
 {
     public partial class RegistratrionForm : Form
     {
+        private readonly Client tcpClient;
+
         public RegistratrionForm()
         {
             InitializeComponent();
+            tcpClient = new Client();
+            this.Load += RegistratrionForm_Load!;
+            //tcpClient.MessageReceived += (sender, e) =>
+            //{
+
+            //};
         }
+
+        private async Task InitializeClientAsync()
+        {
+            try
+            {
+                var ip = "127.0.0.1";
+                var port = 3456;
+                await tcpClient.ConnectAsync(ip, port);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            var loginForm = new Login();
+            var loginForm = new Login(tcpClient);
             this.Hide(); // Hide the Registration form
             loginForm.ShowDialog(); // Open Login form modally
             this.Close(); // Close Registration form after Login form is closed
 
         }
 
-        private void btnRgstr_Click(object sender, EventArgs e)
+        private async Task SendRegisetrRequest(string username, string password)
         {
-            var appForm = new App();
-            this.Hide();
-            appForm.ShowDialog();
-            this.Close();
+            var msg = $"REGISTER {username} {password}";
+            await tcpClient.SendMessageAsync(msg);
+        }
+
+        private async void btnRgstr_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (tbPsw.Text == tbPswConfirm.Text)
+                {
+                    await SendRegisetrRequest(tbLogin.Text, tbPsw.Text);
+                }
+
+                var msg = await tcpClient.ReceiveMessageAsync();
+
+                if (msg == "REGISTER_SUCCESS")
+                {
+                    var appForm = new App(tcpClient);
+                    this.Hide();
+                    appForm.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Login has already taken or credentials are invalid", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private async void RegistratrionForm_Load(object sender, EventArgs e)
+        {
+            await InitializeClientAsync();
         }
     }
 }
