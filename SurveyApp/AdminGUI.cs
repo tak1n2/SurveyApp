116c10ad6bd19ecff6533c8a4d5c28e430e1bf4b
Nikita <avramkin.nikita@gmail.com>
2024-11-25 18:58:32 +0200

    added check

--- SurveyApp/AdminGUI.cs
+++ SurveyApp/AdminGUI.cs
@@ -33,6 +33,18 @@ namespace SurveyApp
                 MessageBox.Show("Please fill out all fields properly.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            for (int i = 0; i < options.Length; i++)
+            {
+                for (int j = i + 1; j < options.Length; j++)
+                {
+                    if (options[i] == options[j])
+                    {
+                        MessageBox.Show($"Duplicate options found: \"{options[i]}\". Options must be unique.",
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
 
             var createCommand = $"CREATE {topic} \"{description}\" {string.Join("|", options)}";
             await tcpClient.SendMessageAsync(createCommand);
